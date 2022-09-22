2ea17bf82c8cd5b915511961d8bc49f8c48659a4
TheEasiestEnemie <hen21060@byui.edu>
2022-09-22 12:50:18 -0600

    My version of hello world

--- hello_world_section_7-jack/Program.cs
+++ hello_world_section_7-jack/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
+
+Console.Write("What is your first name? ");
+string? first_name = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string? last_name = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+
+
+
