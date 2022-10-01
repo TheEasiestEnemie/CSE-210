e421644def49a55cafc6624331cfe255e0c23e24
Gpoulcat <gpoulcat@gmail.com>
2022-10-01 12:02:47 -0600

    Added some code

--- CSE210_HighLow/HighLow.cs
+++ CSE210_HighLow/HighLow.cs
@@ -1,17 +1,28 @@
 ﻿public class HighLow 
 {
     /// Put main logic here!
-    public static void Main(String[] args)
+    public void Main(String[] args)
     {
-
+        int score = 0;
     }
 
     /// Gets a guess from the player. True if "h", false if "l"
     /// Takes no parameters
     /// returns a bool true or false 
-    public GetGuess()
+    public bool GetGuess()
     {
+        Console.Write("What is your guess (h, l");
+        string? guess = Console.ReadLine();
 
+        if (guess == "h")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 
     /// Checks if the player's guess matches the outcome. The outcome 
@@ -19,7 +30,7 @@
     /// Takes the bool guessHigher variable and the outcome
     /// returns True if the comparison matches (false == false or true == true),
     /// otherwise returns false.
-    public IsGuessCorrect(bool guessHigher, bool outcome)
+    public bool IsGuessCorrect(bool guessHigher, bool outcome)
     {
 
     }
@@ -28,7 +39,7 @@
     /// Takes the guess outcome (just IsGuessCorrect in a variable) and
     /// the score to add to.
     /// Returns an int of the new score
-    public ChangePoints(bool guessOutcome, int score)
+    public int ChangePoints(bool guessOutcome, int score)
     {
 
     }
@@ -36,15 +47,31 @@
     /// Checks to see if score is greater than zero. If it isn't, Game is over
     /// Takes the score variable to compare to
     /// Returns a bool True/False. True if score is <= 0
-    public IsGameOver(int score)
+    public bool IsGameOver(int score)
     {
-
+        if (score <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     /// Asks the user if they want to play another round
     /// Takes no parameters
     /// returns a bool True/False. True if answer is yes
-    public PlayAgain()
+    public bool PlayAgain()
     {
-
+        Console.Write("Do you want to play another round?(YES/NO): ");
+        string? play = Console.ReadLine();
+        if (play == "YES")
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 }
