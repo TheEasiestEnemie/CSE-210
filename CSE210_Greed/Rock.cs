93aee56babab699b86a570f6c0ae5e39987ec720
TheEasiestEnemie <hen21060@byui.edu>
2022-11-01 17:08:24 -0600

    Added rock constructor

--- CSE210_Greed/Rock.cs
+++ CSE210_Greed/Rock.cs
@@ -1,4 +1,12 @@
+using Raylib_cs;
+
 public class Rock: Collectable
 {
-    
+    private int pointValue;
+
+    public Rock(int screenWidth)
+    {
+        Collectable rock = new Collectable(screenWidth);
+        rock.model = new Rectangle((int)rock.x, (int)rock.y, rock.size, rock.size);
+    }
 }
