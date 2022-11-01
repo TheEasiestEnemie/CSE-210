47886076a972efe0dc25a434cf65389899e85e06
TheEasiestEnemie <hen21060@byui.edu>
2022-11-01 17:07:53 -0600

    Added constructors

--- CSE210_Greed/Collectable.cs
+++ CSE210_Greed/Collectable.cs
@@ -1,8 +1,25 @@
 public class Collectable: Obj
 {
-    
-    public Collectable() {
-        
-        
+    // This creates an object capable of being collected. 
+    // It doesn't know what the collectable is specifically.
+    // create a rock/gem etc object to call this constructor
+    public Collectable(int screenWidth) 
+    { 
+        Random rand = new Random();
+        Obj collectable = new Obj();
+        collectable.x = (float)Math.Round(rand.NextDouble() * (double)screenWidth);
+        if ((collectable.x + collectable.size) > screenWidth)
+        {
+            collectable.x -= collectable.size;
+        }
+
+        collectable.y = 0;
+    }
+
+    public Collectable() 
+    {
+        Obj collectable = new Obj();
+        collectable.x = 0;
+        collectable.y = 0;
     }
 }
