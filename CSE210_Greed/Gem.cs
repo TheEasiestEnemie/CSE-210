e7e6e4f1fe22c2eb131d36f1a4d957e97e5df77d
TheEasiestEnemie <hen21060@byui.edu>
2022-11-02 15:30:49 -0600

    Mostly Finished Collectable, Gem, and Rock. Had an epiphany.

--- CSE210_Greed/Gem.cs
+++ CSE210_Greed/Gem.cs
@@ -1,4 +1,19 @@
+using Raylib_cs;
+
 public class Gem: Collectable
 {
-    
+    private int pointValue;
+    new Object model;
+    public Gem(int screenWidth) : base(screenWidth)
+    {
+        Collectable gem = new Collectable(screenWidth);
+        pointValue = -1;
+        model = new Rectangle((int)gem.x, (int)gem.y, gem.size, gem.size);
+    }
+
+    public void DrawGem(Object model)
+    {
+        // The x and y for a circle start at the center, not the top left corner
+        Raylib.DrawCircle((int)x + size / 2, (int)y + size / 2, size / 2, color);
+    }
 }
--- CSE210_Greed/Rock.cs
+++ CSE210_Greed/Rock.cs
@@ -3,10 +3,17 @@ using Raylib_cs;
 public class Rock: Collectable
 {
     private int pointValue;
-
-    public Rock(int screenWidth)
+    new Object model;
+    public Rock(int screenWidth) : base(screenWidth)
     {
         Collectable rock = new Collectable(screenWidth);
-        rock.model = new Rectangle((int)rock.x, (int)rock.y, rock.size, rock.size);
+        pointValue = -1;
+        model = new Rectangle((int)rock.x, (int)rock.y, rock.size, rock.size);
+    }
+
+    public void DrawRock()
+    {
+        // The x and y for a circle start at the center, not the top left corner
+        Raylib.DrawCircle((int)x + size / 2, (int)y + size / 2, size / 2, color);
     }
 }
