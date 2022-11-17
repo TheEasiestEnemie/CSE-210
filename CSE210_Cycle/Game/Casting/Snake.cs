b3e4454df81154a2eedffce918ccf8bf539ba878
TheEasiestEnemie <hen21060@byui.edu>
2022-11-17 12:49:44 -0700

    Added a constuctor to snake thats sets a position

--- CSE210_Cycle/Game/Casting/Snake.cs
+++ CSE210_Cycle/Game/Casting/Snake.cs
@@ -19,6 +19,10 @@ namespace Unit05.Game.Casting
         {
             PrepareBody();
         }
+        public Snake(int x, int y)
+        {
+            PrepareBody(x, y);
+        }
 
         /// <summary>
         /// Gets the snake's body segments.
@@ -110,6 +114,22 @@ namespace Unit05.Game.Casting
                 string text = i == 0 ? "8" : "#";
                 Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
 
+                Actor segment = new Actor();
+                segment.SetPosition(position);
+                segment.SetVelocity(velocity);
+                segment.SetText(text);
+                segment.SetColor(color);
+                segments.Add(segment);
+            }
+        }
+        private void PrepareBody(int x, int y)
+        {
+            for (int i = 0; i < Constants.SNAKE_LENGTH; i++)
+            {
+                Point position = new Point(x, y + i * Constants.CELL_SIZE);
+                Point velocity = new Point(0, -1 * Constants.CELL_SIZE);
+                string text = i == 0 ? "8" : "#";
+                Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
                 Actor segment = new Actor();
                 segment.SetPosition(position);
                 segment.SetVelocity(velocity);
